d99afc9828cc057b9961857185bd441cee75aa91
AdiGhadge <adityaghadge233@gmail.com>
2024-02-05 22:53:57 +0530

    Gamejam

---
+++ GameObjects/Gun.go
@@ -0,0 +1,76 @@
+components {
+  id: "gun"
+  component: "/Scripts/Gun.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "enemyBulletFactory"
+  type: "factory"
+  data: "prototype: \"/GameObjects/EnemyLaser.go\"\n"
+  "load_dynamically: false\n"
+  "dynamic_prototype: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "playerBulletFactory"
+  type: "factory"
+  data: "prototype: \"/GameObjects/PlayerLaser.go\"\n"
+  "load_dynamically: false\n"
+  "dynamic_prototype: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "Laser"
+  type: "sound"
+  data: "sound: \"/Assets/Sounds/LaserShot.ogg\"\n"
+  "looping: 0\n"
+  "group: \"sfx\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
